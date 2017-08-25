2dbb21b318f7a959e36251e40d8c54a3a47cf5b6
nadiaivannova <nadiaivannova@gmail.com>
2017-08-25 18:47:58 -0400

    add routes and root, modles and controlers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :activities, :messages, :parks, :trips, :users
+
+root 'parks#index'
+
+
 end
