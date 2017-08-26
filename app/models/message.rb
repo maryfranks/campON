9e8e59011066ce2369316e68a3189d5675645497
nadiaivannova <nadiaivannova@gmail.com>
2017-08-26 17:39:07 -0400

    adjust model validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :trip
 
-  validates :user_id, :message, :trip_id,  presence: true
+  validates :user_id, :message, :trip_id, :session_id, presence: true
 
 end
