cc180aa74ff4b922394f77209058415c75a0d7da
Abby Wilson <abigail.wilson1@outlook.com>
2017-08-28 11:59:17 -0400

    Add application header and footer html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pageshome/contact'
 
+  get 'static_pageshome/contact'
   get 'static_pageshome/about'
 
   resources :trips do
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
 
   resources :parks, only: [:index, :show]
-  resource :user 
+  resource :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
