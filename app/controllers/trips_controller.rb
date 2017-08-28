a073cf59e13c80a3eb13855cafe9e17ce9cd369f
maryfranks <maryfranks@gmail.com>
2017-08-28 12:25:51 -0400

    add new trip form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,22 @@
 class TripsController < ApplicationController
+
+  def new
+    @trip = Trip.new
+    @parks = Park.all 
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+
+def trip_params
+end
+
 end
