c117d4e153df9eda7fb04c29f1ed68af73e9e572
Abby Wilson <abigail.wilson1@outlook.com>
2017-08-28 10:34:13 -0400

    resources added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pageshome/contact'
+
+  get 'static_pageshome/about'
+
+  resources :trips do
+    resources :comments
+  end
+
+  resources :parks, only: [:index, :show]
+  resource :user 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
