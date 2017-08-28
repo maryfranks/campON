7366600c3eaf39e8dff4bc2e54c3cc6ce8c5792b
nadiaivannova <nadiaivannova@gmail.com>
2017-08-28 17:36:32 -0400

    modify sessions and user controler to add user log ins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+   private
+    def current_user
+        @_current_user ||= session[:user_id] &&
+          User.find_by(id: session[:user_id])
+    end
+
 end
