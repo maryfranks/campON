d8b34393e1e306b6f81b5bdd3eb6db1550f594b9
maryfranks <maryfranks@gmail.com>
2017-08-28 15:05:22 -0400

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Park.create(name: "Bitmaker Woods", birding: true, day_use: true, canoeing: true)
+Park.create(name: "CamelCase Beach", birding: true, yurt: true, comfort_station: true, pet_exercise_area: true)
+Park.create(name: "GitHub Harbor", birding: true, rentals: true, walk_in_camping: true, winter_activities: true)
+Park.create(name: "Strong Params Provincial Park", birding: true, park_store: true, camp_cabin: true, car_camping: true)
+
+User.create(name: "Ruby", email: "ruby@email.com", password: "1234", password_confirmation: "1234")
+User.create(name: "Jason", email: "jason@email.com", password: "1234", password_confirmation: "1234")
