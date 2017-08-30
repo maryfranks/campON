8e214d3069952f6b0ab4ed698c3ac9d0a0f3cbaa
nadiaivannova <nadiaivannova@gmail.com>
2017-08-29 21:46:56 -0400

    refactor code to add facilities filter

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -3,16 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var activitiesForm  = document.querySelector('.activities-filter-form')
 var activityCheck   = document.querySelectorAll('.activity')
-
-var activitiesForm  = document.querySelector('.facilities-filter-form')
+var facilitiesForm  = document.querySelector('.facilities-filter-form')
 var facilitiesCheck = document.querySelectorAll('.facilities')
-
-var parks = document.querySelectorAll('.parks')
-
-var filterResults = document.querySelector('#parkPartialList')
+var parks           = document.querySelectorAll('.parks')
+var filterResults   = document.querySelector('#parkPartialList')
 
 
-for (var i = 0; i < activityCheck.length; i++) {
+  for (var i = 0; i < activityCheck.length; i++) {
   activityCheck[i].addEventListener('change', function (event) {
 
     $.ajax({
@@ -22,10 +19,9 @@ for (var i = 0; i < activityCheck.length; i++) {
       dataType: 'html',
     }).done(function(data) {
       filterResults.innerHTML = data
-
     })
-    })
-  }
+  })
+}
 
   for (var i = 0; i < facilitiesCheck.length; i++) {
       facilitiesCheck[i].addEventListener('change', function (event) {
@@ -33,13 +29,13 @@ for (var i = 0; i < activityCheck.length; i++) {
       $.ajax({
         url: 'http://localhost:3000/parks/search',
         method: 'get',
-        data: $(activitiesForm).serialize(),
+        data: $(facilitiesForm).serialize(),
         dataType: 'html',
       }).done(function(data) {
         filterResults.innerHTML = data
       })
-      })
-    }
+    })
+  }
 
 
 })
