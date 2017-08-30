ecd4387cc8f3bdcb4b044fb1846db108efea3d33
nadiaivannova <nadiaivannova@gmail.com>
2017-08-29 21:14:19 -0400

    add new permits to whitelisting

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,10 +1,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-var activityCheck = document.querySelectorAll('.activity')
+var activitiesForm  = document.querySelector('.activities-filter-form')
+var activityCheck   = document.querySelectorAll('.activity')
+
+var activitiesForm  = document.querySelector('.facilities-filter-form')
+var facilitiesCheck = document.querySelectorAll('.facilities')
+
 var parks = document.querySelectorAll('.parks')
-var activityResults = document.querySelector('#parkPartialList')
-var activitiesForm = document.querySelector('.activities-filter-form')
+
+var filterResults = document.querySelector('#parkPartialList')
 
 
 for (var i = 0; i < activityCheck.length; i++) {
@@ -16,9 +21,25 @@ for (var i = 0; i < activityCheck.length; i++) {
       data: $(activitiesForm).serialize(),
       dataType: 'html',
     }).done(function(data) {
-      activityResults.innerHTML = data
+      filterResults.innerHTML = data
 
     })
     })
   }
+
+  for (var i = 0; i < facilitiesCheck.length; i++) {
+      facilitiesCheck[i].addEventListener('change', function (event) {
+
+      $.ajax({
+        url: 'http://localhost:3000/parks/search',
+        method: 'get',
+        data: $(activitiesForm).serialize(),
+        dataType: 'html',
+      }).done(function(data) {
+        filterResults.innerHTML = data
+      })
+      })
+    }
+
+
 })
