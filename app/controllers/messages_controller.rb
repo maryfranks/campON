f84bbe68d088bb497257cad6786d216ab853b194
Martine Gagne <martine.e.gagne@gmail.com>
2017-08-31 17:37:35 -0400

    begin work on delete/edit message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,32 +15,26 @@ class MessagesController < ApplicationController
     @message.message = params[:message][:message]
 
     if @message.save
-      flash[:success] = "Message successfully added"
       redirect_to '/trips/:id'
-
     end
 
     def search
       @messages = Message.where(search_params)
       render partial: 'message_display'
     end
+
+    def update
+      @message.user_id = current_user.id
+      @message.trip_id = params[:trip_id]
+      @message.message = params[:message][:message]
+
+      if @message.save
+        redirect_to trip_path
+      else
+        render 'trips/show'
+      end
+    end
   end
-    #
-    # def update
-    #   @message = Message.find(params[:id])
-    #   trip_id = params[:trip_id]
-    #   @trip = Trip.find(trip_id)
-    #   @messages = @messages.message
-    #
-    #   @message.message = params[:message][:trip]
-    #   @message.trip_id = trip_id
-    #
-    #   if @message.save
-    #     redirect_to trip_path
-    #   else
-    #     render 'trips/show'
-    #   end
-    # end
     #
     # def destroy
     #   @message = Message.find(params[:id])
