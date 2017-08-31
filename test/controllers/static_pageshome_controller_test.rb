b8fc79bf4fcf3d8bea0ea0f2640914297712eca8
nadiaivannova <nadiaivannova@gmail.com>
2017-08-31 14:11:04 -0400

    add validation test for user

--- test/controllers/static_pageshome_controller_test.rb
+++ test/controllers/static_pageshome_controller_test.rb
@@ -1,14 +1,14 @@
-require 'test_helper'
-
-class StaticPageshomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get contact" do
-    get static_pageshome_contact_url
-    assert_response :success
-  end
-
-  test "should get about" do
-    get static_pageshome_about_url
-    assert_response :success
-  end
-
-end
+# require 'test_helper'
+#
+# class StaticPageshomeControllerTest < ActionDispatch::IntegrationTest
+#   test "should get contact" do
+#     get static_pageshome_contact_url
+#     assert_response :success
+#   end
+#
+#   test "should get about" do
+#     get static_pageshome_about_url
+#     assert_response :success
+#   end
+#
+# end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   test "user fixtures" do
     assert_equal 4, User.count
   end
+
+
+test "user should not save without name" do
+  user = User.new(email: 'nadia@boo.ca', password: '1234')
+  assert_not user.save
 end
 
-user
+test "user should not save without password" do
+  user = User.new(email: 'nadia@boo.ca', name: 'nadia')
+  assert_not user.save
+end
+
+test "user should not save without email" do
+  user = User.new(name: 'nadia', password: '1234')
+  assert_not user.save
+end
+
+
+# test "user can login" do
+#
+# end
+#
+# test "user can logout" do
+#
+# end
+
+end
