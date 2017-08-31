c177388bc6590620d5af3d8cb6c8f8c3a83294a0
nadiaivannova <nadiaivannova@gmail.com>
2017-08-31 16:23:15 -0400

    update js for messages

--- app/assets/javascripts/trip.js
+++ app/assets/javascripts/trip.js
@@ -1,22 +1,17 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   var displayMessage = document.querySelector('.display_message');
-//   var displayForm = document.querySelector('.message_form');
-//
-//   displayForm.addEventListener('submit', function(event){
-//     event.preventDefault()
-//     $.ajax({
-//       url: 'http://localhost:3000/message/search',
-//       method: 'get',
-//
-//       dataType: 'html',
-//   }).done(function(data) {
-//     console.log(data);
-//     displayMessage.innterHTML = data
-//
-// // listen for submit click
-// // once it submits on form, we want to capture that messsage info
-// // create dom object
-// // append message
-//   });
-// });
-// });
+document.addEventListener("DOMContentLoaded", function() {
+  var displayMessage = document.querySelector('.display_message');
+  var displayForm = document.querySelector('.message_form');
+
+  displayForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    $.ajax({
+      url: 'http://localhost:3000/message/search',
+      method: 'get',
+      data:
+      dataType: 'html',
+    }).done(function(data) {
+    console.log(data);
+    displayMessage.innterHTML = data
+    });
+  });
+});
