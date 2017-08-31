73893fb86682e6d5026d4423f797b87d68dd0fa3
maryfranks <maryfranks@gmail.com>
2017-08-31 10:16:01 -0400

    trip test skeleton

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,7 +1,66 @@
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
+
   test "trip fixtures" do
-    assert_equal 3, Trip.count 
+    assert_equal 3, Trip.count
   end
+
+  test "user can create trip" do
+
+  end
+
+  test "trip must have name" do
+    # When you save a trip with a name the test passes
+  end
+
+  test "trip name validation"
+  # when you save a trip without a name it fails and the test passes
+  end
+
+  test "user can comment on trip" do
+
+  end
+
+  test "trip view displays all comments" do
+
+  end
+
+  test "add park to trip" do
+
+  end
+
+  test "add start date to trip" do
+
+  end
+
+  test "add end date to trip" do
+
+  end
+
+  test "user can add users to trip" do
+    skip
+  end
+
+  test "user can change booked to true" do
+
+  end
+
+  test "trip displays users" do
+
+  end
+
+  test "trip displays dates" do
+
+  end
+
+  test "trip displays booked checkmark" do
+
+  end
+
+  test "trip displays park" do
+    
+  end
+
+
 end
