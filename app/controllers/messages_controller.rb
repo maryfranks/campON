9f4f32e18d4422ba520890166da55899278453c7
Martine Gagne <martine.e.gagne@gmail.com>
2017-09-01 15:20:41 -0400

    add delete/edit message buttons

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,33 +16,39 @@ class MessagesController < ApplicationController
 
     if @message.save
       redirect_to "/trips/#{params[:trip_id]}"
-
     end
+  end
 
     def search
       @messages = Message.where(search_params)
       render partial: 'message_display'
     end
 
+    def edit
+      @trip =Trip.find(params[:trip_id])
+      @message = Message.find(params[:id])
+    end
+
     def update
+      @trip = Trip.find(params[:trip_id])
+      @message = Message.find(params[:id])
       @message.user_id = current_user.id
       @message.trip_id = params[:trip_id]
       @message.message = params[:message][:message]
 
       if @message.save
-        redirect_to trip_path
+        redirect_to "/trips/#{params[:trip_id]}"
       else
         render 'trips/show'
       end
     end
-  end
-    #
-    # def destroy
-    #   @message = Message.find(params[:id])
-    #   @message.destroy
-    #   redirect_to trip_path
-    #   flash[:notice] = "Message has been successfully deleted!"
-    # end
+
+    def destroy
+      @message = Message.find(params[:id])
+      @message.destroy
+      redirect_to "/trips/#{params[:trip_id]}"
+      flash[:notice] = "Message has been successfully deleted!"
+    end
 
   private
     def message_params
