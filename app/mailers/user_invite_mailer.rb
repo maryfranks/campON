7bf074f66c87978c862625f34dc81fd2cb373c31
nadiaivannova <nadiaivannova@gmail.com>
2017-09-04 15:09:23 -0400

    basic sendgrid code no functionality

---
+++ app/mailers/user_invite_mailer.rb
@@ -0,0 +1,12 @@
+class UserInviteMailer < ApplicationMailer
+
+    default :from => 'martine.e.gagne@gmail.com'
+
+    # send a signup email to the user, pass in the user object that   contains the user's email address
+    def send_signup_email(user)
+      @user = user
+      mail( :to => @user.email,
+      :subject => 'Thanks for signing up for our amazing app' )
+    end
+
+end
