4a9a76ab9871876129a3477d59e97a04da04b1de
maryfranks <maryfranks@gmail.com>
2017-09-05 13:36:19 -0400

    update and delete todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,4 +14,29 @@ class TodosController < ApplicationController
     end
   end
 
+  def edit
+    @trip =Trip.find(params[:trip_id])
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:trip_id])
+    @todo = Todo.find(params[:id])
+    @todo.trip_id = params[:trip_id]
+    @todo.text = params[:todo][:text]
+
+    if @todo.save
+      redirect_to "/trips/#{params[:trip_id]}"
+    else
+      render 'trips/show'
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to "/trips/#{params[:trip_id]}"
+    flash[:notice] = "ToDo has been successfully deleted!"
+  end
+
 end
