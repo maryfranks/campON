e504e293290c3b46beb06c1de8c018594aae4a14
maryfranks <maryfranks@gmail.com>
2017-09-05 13:20:27 -0400

    save todo and show all todos for trip on trip show page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,17 @@
 class TodosController < ApplicationController
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new
+    @todo.trip_id = params[:trip_id]
+    @todo.text = params[:todo][:text]
+
+    if @todo.save
+      redirect_to "/trips/#{params[:trip_id]}"
+    end
+  end
+
 end
