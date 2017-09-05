d6dd045a96b1a9e58c16fd6150b019076baf0b85
maryfranks <maryfranks@gmail.com>
2017-09-05 17:35:14 -0400

    add testing skeleton

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -4,4 +4,22 @@ class TodoTest < ActiveSupport::TestCase
   test "todo fixtures" do
     assert_equal 3, Todo.count
   end
+
+  test "todo saves with only a name" do
+    # test passes when the todo saves
+    # trip = Trip.new
+    # trip.name = "New Trip"
+    # assert trip.save
+  end
+
+  test "todo does not save without name" do
+    # test passes when the save fails
+    # trip = Trip.new()
+    # refute trip.save
+  end
+
+  test "can add user to todo" do
+
+  end
+
 end
