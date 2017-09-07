ab4bf754d9057e376df53e5183e95426efccdff8
maryfranks <maryfranks@gmail.com>
2017-09-07 15:07:46 -0400

    add skeleton for message controller tests

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -5,5 +5,29 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @message = messages(:one)
   end
-  
+
+  test "current trip" do
+    skip
+  end
+
+  test "new" do
+    skip
+  end
+
+  test "create" do
+    skip
+  end
+
+  test "search" do
+    skip
+  end
+
+  test "update" do
+    skip
+  end
+
+  test "destroy" do
+    skip
+  end
+
 end
