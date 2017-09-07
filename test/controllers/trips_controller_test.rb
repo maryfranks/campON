be4a5a1df9797bd3b37491dbb23f3761936dca06
maryfranks <maryfranks@gmail.com>
2017-09-07 13:53:30 -0400

    add skipped tests for trip update and destroy

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -29,21 +29,24 @@ class TripsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "updated", @trip.name
   end
 
-  # test "should destroy trip" do
-  #   assert_difference('Trip.count', -1) do
-  #   delete trip_url(@trip)
-  #   end
-  #   user = users(:martine)
-  #   assert_redirected_to user_path(user) WON'T REDIRECT TO USER SHOW
-  # end
+  test "should destroy trip" do
+    skip
+    # sign_in_as users(:martine)
+    # assert_difference('Trip.count', -1) do
+    # delete trip_url(@trip)
+    # end
+    # assert_redirected_to user_path
+  # WON'T REDIRECT TO USER SHOW
+  end
 
-  # test "should create trip" do
+  test "should create trip" do
+    skip
   #   assert_difference('Trip.count') do
   #     post trips_url, params: { trip: { name: 'Really Fun Trip!' } }
   #     users(:martine) >> JOIN TABLE!!!
   #   end
   #
   #   assert_redirected_to trip_path(Trip.last)
-  # end
+  end
 
 end
