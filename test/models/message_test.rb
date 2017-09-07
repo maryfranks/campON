c29b7e570e54004484b5a0484365b8db09d6e63d
Martine Gagne <martine.e.gagne@gmail.com>
2017-09-07 11:18:03 -0400

    create message tests

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -2,25 +2,50 @@ require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
 
-  test "message is saved with a trip_id" do
+  test "message does not saves without  text" do
     message = Message.new
-    message.trip_id = 1
+    message.user = users(:martine)
+    message.trip = trips(:one)
+    message.message = "Please do save"
     assert message.save
   end
 
-  test "message does not save without a trip_id" do
+  test "message saves with a text" do
+    message = Message.new
+    message.trip = trips(:one)
+    message.user = users(:martine)
+    message.message = "Please do save"
+    assert message.save
   end
 
-  test "message is saved with a user_id" do
+  test "message is saved with a user" do
+    message = Message.new
+    message.trip = trips(:one)
+    message.message = "Please do save"
+    refute message.save
   end
 
-  test "message does not save without a user_id" do
+  test "message is saved without a trip" do
+    message = Message.new
+    message.user = users(:martine)
+    message.message = "Please do save"
+    refute message.save
   end
 
-  test "message can be edited" do
+  test "message does not save without a user" do
+    message = Message.new
+    message.trip = trips(:one)
+    message.user = users(:martine)
+    message.message = "Please do save"
+    assert message.save
   end
 
-  test "message cannot be deleted" do
+  test "message does not save without a trip" do
+    message = Message.new
+    message.user = users(:martine)
+    message.trip = trips(:one)
+    message.message = "Please do save"
+    assert message.save
   end
 
 end
