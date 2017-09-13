22aac538af1aeebd06f2e73036bf9ad1c607f867
maryfranks <maryfranks@gmail.com>
2017-09-13 16:48:48 -0400

    test update message

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -2,15 +2,15 @@ require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
 
+  include SignInHelper
+
   setup do
     @message = messages(:one)
+    sign_in_as users(:martine)
+    @trip = trips(:three)
   end
 
   test "current trip" do
-    skip
-  end
-
-  test "new" do
     skip
   end
 
@@ -20,6 +20,11 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
 
   test "search" do
     skip
+  end
+
+  test "edit" do
+      get edit_trip_message_url(@message.id, @trip.id)
+      assert_response :success
   end
 
   test "update" do
