48990e1c940c57710cd41677b90e292fdddb1e22
maryfranks <maryfranks@gmail.com>
2017-09-15 13:00:46 -0400

    setup user controller test and test for new and show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  include SignInHelper
+
+  setup do
+    @trip = trips(:one)
+    @user = users(:martine)
+    @trip.users << users(:martine)
+    sign_in_as users(:martine)
+  end
+
+  test "should get new" do
+    get new_user_url
+    assert_response :success
+  end
+
+  test "should get show when user is signed in" do
+    get user_url(@user)
+    assert_response :success
+  end
+
+  test "should create user" do
+    skip
+    assert_difference('User.count') do
+      post user_url, params: { user: { email: 'user@email.com', password: "1234", password_confirmation: "1234" } }
+    end
+
+    assert_redirected_to user_path
+    assert_equal 'Account successfully created!', flash[:notice]
+  end
+
 end
